d17761f8cf2194ab348e0cf8cc2c0d84bcc534c5
sagnikc395 <sagnikchatterjee395@gmail.com>
2023-12-10 14:00:22 +0530

    src: added index-signatures.ts

---
+++ etc/ts-foundations/index-signatures.ts
@@ -0,0 +1,13 @@
+type GroceryList = {
+  [type: string]: number;
+};
+
+type InappropriateActionBySituation = {
+  [type: string]: string[];
+};
+
+type CharactersById = {
+  [type: number]: {
+    [type: string]: number | string;
+  };
+};
