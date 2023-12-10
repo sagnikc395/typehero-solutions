f99983597d9aa862fcbf2195ebbd0d075eb2b403
sagnikc395 <sagnikchatterjee395@gmail.com>
2023-12-10 11:35:46 +0530

    docs: added explannation

--- day2.ts
+++ day2.ts
@@ -5,5 +5,6 @@ all the names of the various different cookies.
 check from cookieInventory
 
 */
-
+//making a generic function that takes in a object and
+// returns a union type of all the keys of the object as strings.
 type CookieSurveyInput<T> = Extract<keyof T,string>;
