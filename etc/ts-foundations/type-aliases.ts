2c31abc56c9e5bcb30463590411cfaa1caa529b6
sagnikc395 <sagnikchatterjee395@gmail.com>
2023-12-10 12:13:03 +0530

    src: added type aliases

---
+++ etc/ts-foundations/type-aliases.ts
@@ -0,0 +1,18 @@
+// We completed the first alias (`Name`) for you to see as an example
+type Name = string;
+
+// Now try replacing `unknown` with a primitive data type that might be appropriate for `Year`
+type Year = number;
+
+type IsOperational = boolean;
+type Count = number;
+
+type Kilograms = number;
+
+type Payload = {
+  name: Name;
+  mass: Kilograms;
+  // the tests show that you need a `mass` property here
+  // but first you might need to create an alias for `Kilograms`
+  // because that's the value of `mass`
+};
