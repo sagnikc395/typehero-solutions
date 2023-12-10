095676f0ea6edae4a92ffc8e4ad52467a28b6baf
sagnikc395 <sagnikchatterjee395@gmail.com>
2023-12-10 12:08:00 +0530

    src: completed primitive-data-types

---
+++ etc/ts-foundations/primitive-data-types.ts
@@ -0,0 +1,22 @@
+const artistName: string = "Frank Zappa";
+const age: number = 52;
+const playSong = (artistName: string, year: number) => {
+  return `${artistName} was released in the year ${year}`;
+};
+
+interface Musician {
+  artistName: string;
+  age: number;
+  deceased: boolean;
+  // add the rest
+}
+
+const musicianInfo = ({ artistName, age, deceased }: Musician) => {
+  return `${artistName}, age ${age}${deceased ? " (deceased)" : ""}`;
+};
+
+musicianInfo({
+  artistName,
+  age,
+  deceased: true,
+});
